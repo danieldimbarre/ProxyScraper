ec477cca6bf790603abaad31290db789112e3a9c
GhostyCeh <41862824+GhostyCeh@users.noreply.github.com>
2020-09-15 11:05:34 -0500

    Update Gathering.py

--- Gathering.py
+++ Gathering.py
@@ -1,7 +1,15 @@
-import asyncio, os, colorama
+import asyncio, os, colorama, re, time
 from proxybroker import Broker
 from colorama import *
 
+os.system("rm -rf proxies.txt")
+os.system("cat proxies.txt >> proxies.txt")
+
+def delay():
+	os.system("sh blacklist.sh")
+	print(">> Waiting 10 minutes to renew proxies")
+	time.sleep(600)
+	Scraper()
 
 async def save(proxies, filename):
     with open(filename, 'w+') as f:
@@ -18,14 +26,47 @@ async def save(proxies, filename):
 def main():
     proxies = asyncio.Queue()
     broker = Broker(proxies)
-    tasks = asyncio.gather(broker.find(types=['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'], limit= 99999),
-                           save(proxies, filename='Gathering.txt'))
+    tasks = asyncio.gather(broker.find(types=['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'], limit= 5),
+                           save(proxies, filename='proxy.txt'))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(tasks)
+    delay()
+	
+def Scraper():
+	os.system("wget -O PShttp.txt https://api.proxyscrape.com?request=getproxies&proxytype=http&timeout=10000&country=all&anonymity=all&ssl=all")
+	os.system("wget -O PSsocks4.txt https://api.proxyscrape.com?request=getproxies&proxytype=socks4&timeout=10000&country=all")
+	os.system("wget -O PSsocks5.txt https://api.proxyscrape.com?request=getproxies&proxytype=socks4&timeout=10000&country=all")
+
+	Scraper = open("ScraperSocks.txt",'wb')
+	try:
+		os.system("wget -O proxyS4.txt https://www.proxy-list.download/api/v1/get?type=socks4")
+		os.system("wget -O proxyS5.txt https://www.proxy-list.download/api/v1/get?type=socks5")
+	except:
+		print("A")
+		pass
+	try:
+		os.system("wget -O psS4.txt https://www.proxyscan.io/download?type=socks4")
+		os.system("wget -O psS5.txt https://www.proxyscan.io/download?type=socks5")
+	except:
+		print("B")
+		pass
+	try:
+		os.system("wget -O SpeedS4.txt https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt")
+		os.system("wget -O Speed5.txt https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt")
+	except:
+		print("C")
+		pass
+
+	os.system("cat proxyS4.txt >> proxies.txt && cat proxyS5.txt >> proxies.txt && cat psS4.txt >> proxies.txt && cat SpeedS4.txt >> proxies.txt && cat Speed5.txt >> proxies.txt")
+	with open("proxies.txt") as O, open("proxy.txt","w+") as B:
+        	for line in O:
+                	word = ':'
+                	replace_with = ''
+                	occurrance  = 1
+                	B.write(word.join(line.split(word)[:occurrance]) + "\n")
+	main()
+
+Scraper()
 
 
-if __name__ == '__main__':
-    print("  -  Proxy Gathering  -     ")
-    print("     CTRL + C To STOP       ")
-    print("     File = Gathering.txt       ")
-    main()
+
